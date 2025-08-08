d7f667df02bbb53baeaeb62e68739429e3c599e5
omkargiri2293 <omkargiri2293@gmail.com>
2025-08-09 00:09:27 +0530

    2

--- Ex_8.c
+++ Ex_8.c
@@ -1,3 +1,35 @@
 /*
 Convert Celsius to Fahrenheit
 */
+
+// multiply by 1.8 ( 9/5 ) and add 32
+
+#include <stdio.h>
+
+int main(void)
+{
+    double c = 0;
+    double f = 0;
+
+    double start = 0;
+    double stop = 0;
+    double step = 0;
+
+    printf("enter start celsius value ");
+    scanf("%lf", &start);
+
+    printf("enter stop celsius value ");
+    scanf("%lf", &stop);
+
+    printf("enter step value ");
+    scanf("%lf", &step);
+    printf("Celsius         Fahrenheit");
+    for (int i = start; i <= stop; i += step)
+    {
+
+        f = (c * 1.8) + 32;
+        printf("%8.2lf %8.2lf", c, f);
+    }
+
+    return 0;
+}
